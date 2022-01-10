3fd49746ef7744bb6b83703ae8e141644a72a643
MrJaroo <mishadolininma@mail.ru>
2022-01-10 13:40:26 +0400

    'dz01'

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Message from "./Message";
 
-const messageData = {
+export type MessageDataType = {
+    avatar: string
+    name:string
+    message:string
+    time:string
+}
+
+const messageData:MessageDataType = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Artem',
+    message: 'npm star нажимал',
     time: '22:00',
 }
 
@@ -13,14 +21,14 @@ function HW1() {
             <hr/>
             homeworks 1
 
-            {/*should work (должно работать)*/}
+            should work (должно работать)
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {MessageDataType} from "./HW1";
+import s from './Message.module.css'
 
-function Message() {
+function Message(props: MessageDataType) {
     return (
-        <div>
+        <div className={s.container}>
+            <div className={s.avatarContainer}>
+                <img className={s.avatar} src={props.avatar} alt="logo"/>
+            </div>
+            <div className={s.containerMessage}>
+                <div className={s.nameAndMessage}>
+                    <div className={s.name}>{props.name}</div>
+                    <div className={s.message}> {props.message}</div>
+                    <div className={s.emptiness}></div>
+                </div>
+                <div className={s.time}>{props.time}</div>
+            </div>
 
         </div>
     )
